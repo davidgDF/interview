0322f49553d75a9b9aa784d9bdba41103ade6740
David Alejandro Garcia Lopez <33735098+davidgDF@users.noreply.github.com>
2021-03-05 10:19:49 -0600

    First Commit

--- test.js
+++ test.js
@@ -1,46 +1,5 @@
 var assert = require('assert');
 
-class InventoryList {
-    properties = {};
-    previousPropertiesState = [];
-
-    constructor(obj) {
-      if(obj)
-        this.properties = obj;
-    }
-
-    add(obj, amount = 1) {
-      this.previousPropertiesState.push({...this.properties});
-      
-      if(this.properties[obj]) {
-        this.properties[obj] = this.properties[obj] + amount;
-      } else {
-        this.properties[obj] = amount;
-      }
-    }
-
-    getList() {
-      return this.properties;
-    }
-
-    remove(obj, amount = 1) {
-      this.previousPropertiesState.push({...this.properties});
-
-      var amount = this.properties[obj] - amount;
-      
-      if(amount > 0){
-        this.properties[obj] = amount
-      } else {
-        delete this.properties[obj]
-      }
-    }
-
-    undo (){
-      var previous = this.previousPropertiesState.pop();
-      this.properties = previous;
-    }
-} 
-
 describe('InventoryList', function () {
   it('can add items', function () {
     var list = new InventoryList();
